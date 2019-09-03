07038f8dd68e809648488edd342bf7297357f399
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2019-09-03 13:33:13 +0600

    change databse in models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var MongoClient = require('mongodb').MongoClient;
-var mongoClient = new MongoClient("mongodb://localhost:27017/", { useNewUrlParser: true });
+var mongoClient = new MongoClient("mongodb+srv://pavel-alpinweb:picK28611@myfirstcluster-3rdem.azure.mongodb.net/clientstdata", { useNewUrlParser: true });
 
 module.exports = mongoClient;
